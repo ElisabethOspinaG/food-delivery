1d4f66cf1f4918c917d9f5a369123ecaf014c646
angie moreno <guaiagaleria@gmail.com>
2023-06-19 14:23:08 -0500

    creando order y parando para hacer merge con develop

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../style/order.scss"
+const Order = () => {
+    return (
+        <div className='order'>
+            <button className="order__button">&lt; &nbsp;&nbsp;</button>
+<h1>Order</h1>
+        </div>
+    )
+}
+
+export default Order
